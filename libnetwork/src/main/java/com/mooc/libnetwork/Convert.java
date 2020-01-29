25040399ebfed72aa38435a7f97437bacb5840d8
Yiding He <yhe@truxnow.com>
2020-01-29 12:06:10 -0500

    Init common and network libs.

---
+++ libnetwork/src/main/java/com/mooc/libnetwork/Convert.java
@@ -0,0 +1,9 @@
+package com.mooc.libnetwork;
+
+import java.lang.reflect.Type;
+
+public interface Convert<T> {
+    T convert(String response, Type type);
+
+    T convert(String response, Class claz);
+}
